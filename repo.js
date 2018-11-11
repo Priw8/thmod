ba00f54170b9992130a2bb7cb3a003459d25ba89
Priw8 <priwe8@gmail.com>
2018-11-11 19:02:28 +0100

    test
    still not sure how thcrap works

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "priwe8@gmail.com",
+	"id": "Priw8",
+	"patches": {
+		"th12tsuba": "Replaces Reimu with Tsubakura from the Len'en project. Features custom dialogues, endings, as well as 2 custom shottypes (.sht modifications)."
+	},
+	"servers": [
+		"https://github.com/Priw8/thmod"
+	],
+	"title": "Priw8's mods"
+}
---
+++ th12tsuba/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"id": "th12tsuba",
+	"servers": [
+		"https://github.com/Priw8/thmod/th12tsuba/"
+	],
+	"title": "Replaces Reimu with Tsubakura from the Len'en project. Features custom dialogues, endings, as well as 2 custom shottypes (.sht modifications)."
+}
