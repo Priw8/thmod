f2b8be14a1a66e4c6d000d6e7ab6e5e7fdcd8ed3
Priw8 <priwe8@gmail.com>
2018-11-11 19:08:35 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,11 +1,11 @@
 {
-	"contact": "priwe8@gmail.com",
+	"contact": "Priw8#9873",
 	"id": "Priw8",
 	"patches": {
 		"th12tsuba": "Replaces Reimu with Tsubakura from the Len'en project. Features custom dialogues, endings, as well as 2 custom shottypes (.sht modifications)."
 	},
 	"servers": [
-		"https://github.com/Priw8/thmod"
+		"https://raw.githubusercontent.com/Priw8/thmod/master/"
 	],
 	"title": "Priw8's mods"
 }
