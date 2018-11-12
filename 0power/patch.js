b53d37f928533902fa779cf7aa4d907ced73d247
Priw8 <priwe8@gmail.com>
2018-11-12 11:54:51 +0100

    0 power part 1

---
+++ 0power/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "0power",
+	"servers": [
+		"https://raw.githubusercontent.com/Priw8/thmod/master/0power/"
+	],
+	"title": "(TH10-TH16) locks player shottype at 0 power, ignoring power items. For masochists."
+}
--- repo.js
+++ repo.js
@@ -2,6 +2,7 @@
 	"contact": "Priw8#9873",
 	"id": "Priw8",
 	"patches": {
+		"0power": "(TH10-TH16) locks player shottype at 0 power, ignoring power items. For masochists.",
 		"th12tsuba": "(TH12) Replaces Reimu with Tsubakura from the Len'en project. Features custom dialogues, endings, as well as 2 custom shottypes (.sht modifications).",
 		"th14sanae": "(TH14) Replaces ReimuB with Sanae shot from LoLK (not in menus)",
 		"th15sakuya": "(TH15) Replaces Reisen with Sakuya (not in menus)",
