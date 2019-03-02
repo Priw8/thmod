3789ef12411aa1ee3a56a5322bd48f150072d643
Priw8 <priwe8@gmail.com>
2019-03-02 18:54:21 +0100

    loddk extra update

---
+++ LoDDK/th15.js
@@ -0,0 +1,17 @@
+{
+	"binhacks": {
+		"st07_0power": {
+			"expected": "890D40744E00",
+			"code": "909090909090",
+			"addr": "0x43C353",
+			"title": "Start extra stage with 0 power"
+		},
+		"0_min_power": {
+			"expected": "3B0D48744E000F4C0D48744E00",
+			"code": "9031C039C10F4CC89090909090",
+			"addr": "0x4582FC",
+			"title": "Changes minimum power level from 1.0 to 0.0"
+		}
+	},
+	"title": "Legacy of Double Dealing Kingdom"
+}
