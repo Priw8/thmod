0e352450947d19ac0779d575eb4e1a15e27266be
Priw8 <priwe8@gmail.com>
2019-04-01 22:27:42 +0200

    April fools 2019 meme mod

---
+++ th16mystia/patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin/",
+		"nmlgc/western_name_order/",
+		"thpatch/lang_en/"
+	],
+	"id": "th16mystia",
+	"servers": [
+		"https://raw.githubusercontent.com/Priw8/thmod/master/th16mystia/"
+	],
+	"title": "(TH16) Replaces Cirno with Mystia (not in menus yet) (April fools 2019 mod)"
+}
